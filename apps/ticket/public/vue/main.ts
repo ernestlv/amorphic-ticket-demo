8f8332db9ddea7dea55cfa2be605e664e6009b1c
Lei Niu <lei@havenlife.com>
2017-03-31 08:46:17 -0400

    Move amporphic/window services to the top level

--- apps/ticket/public/vue/main.ts
+++ apps/ticket/public/vue/main.ts
@@ -1,6 +1,11 @@
 import * as Vue from 'vue';
 // import injector from 'vue-inject';
 import App from './App';
+import {AmorphicService} from './amorphic.service';
+import {WindowService} from './window.service';
+import {amorphicService, windowService} from './constants';
+let windowServiceInstance = new WindowService();
+let amorphicServiceInstance = new AmorphicService(windowServiceInstance);
 
 // import './styles/style.scss'
 
@@ -13,6 +18,12 @@ new Vue({
     // data: {
     //     controller: injector.get('amorphicService').controller
     // },
+    provide () {
+        return {
+            [windowService]: windowServiceInstance,
+            [amorphicService]: amorphicServiceInstance
+        }
+    },
     // // store,
     render: h => h(App)
 });
